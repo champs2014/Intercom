53584103ad6f7b76278a771086a53c0c6707bb13
unknown <magalhgu@MAGALHGU.americas.hpqcorp.net>
2016-05-07 16:50:01 -0300

    deleted files.

--- Intercombkp/java_workspace/GreatCircle/src/great/circle/json/StartApplication.java
+++
@@ -1,9 +0,0 @@
-package great.circle.json;
-
-public class StartApplication {
-
-	public static void main(String[] args) {
-		GreatCircle greatCircle = new GreatCircle();
-		greatCircle.printUsers();
-	}
-}
